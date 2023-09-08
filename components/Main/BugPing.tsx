da5911edf19c595ea08fd920255c793911e33c91
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-09-08 20:01:16 +0300

    ping queue

--- components/Main/BugPing.tsx
+++ components/Main/BugPing.tsx
@@ -4,6 +4,7 @@ import { pingBug } from "@/lib/pingBug";
 import { Bug, User as UserDB, Share } from "@prisma/client";
 import { User } from "next-auth";
 import React, { useEffect } from "react";
+import { toast } from "react-hot-toast";
 
 interface BugPingProps {
   bug: Bug & {
@@ -19,11 +20,11 @@ const BugPing: React.FC<BugPingProps> = ({user,bug}) => {
     const socket = useSocketStore(state=>state.socket);
     
     
-    
     return (
     <div>
       <button onClick={()=>{
         pingBug({user,bug,socket});
+        toast.success("Successfully queued !");
       }} className="border-2 border-green-500 font-medium hover:bg-green-500 duration-150 p-1 rounded-md text-[.9em]">
         ping
       </button>
--- components/Main/PingContainer.tsx
+++ components/Main/PingContainer.tsx
@@ -21,25 +21,47 @@ interface PingedBug{
 }
 
 const PingContainer = () => {
-    const [pingedBug,setPingedBug] = useState<PingedBug>();
+    const [queue,setQueue] = useState<Array<PingedBug>>([]);
     const socket = useSocketStore(state=>state.socket);
 
     useEffect(()=>{
       if(!socket) return;
       socket.on("pingBug_client",payload=>{
-        setPingedBug({bug:payload.bug});
+        setQueue(prev=>[...prev,{bug:payload.bug}]);
       });
+    
     },[socket]);
 
+    useEffect(()=>{
+      if(queue.length>0){
+        const id = setInterval(()=>{
+          setQueue(prev=>prev.slice(1));
+        },5000);
+  
+        return ()=>{
+          clearInterval(id);
+        }
+      }
+    },[queue]);
+   
   return (
     <div>
-        <div className="flex">
-            <UserProfile username={pingedBug?.bug.user.name} image={pingedBug?.bug.user.image}/>
+      {queue.length===0 ? 
+        <div>
+          <p className="text-center text-gray-600 text-sm italic">no current pings. wow :)</p>
         </div>
-        <div className="pl-2">
-            <h3 className="max-w-[120px] overflow-hidden truncate">{pingedBug?.bug.title}</h3>
-            <p className="max-w-[150px] truncate overflow-hidden text-gray-500 text-sm">{pingedBug?.bug.description}</p>
+        :
+        <div className="">
+          <div className="flex">
+              <UserProfile username={queue[0]?.bug.user.name} image={queue[0]?.bug.user.image}/>
+          </div>
+          <div className="pl-2">
+              <h3 className="max-w-[120px] overflow-hidden truncate">{queue[0]?.bug.title}</h3>
+              <p className="max-w-[150px] truncate overflow-hidden text-gray-500 text-sm">{queue[0]?.bug.description}</p>
+          </div>
         </div>
+      }
+        
     </div>
   )
 }
