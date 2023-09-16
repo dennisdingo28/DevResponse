0b83d8b1910d2f46a15b22155431a4bf3adf011e
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-09-16 21:22:49 +0300

    login redirect for auth providers

--- app/(routes)/authenticate/components/LoginProviders.tsx
+++ app/(routes)/authenticate/components/LoginProviders.tsx
@@ -2,14 +2,22 @@
 
 import ButtonIcon from "@/components/ui/ButtonIcon";
 import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import {FaGithub, FaGoogle} from "react-icons/fa";
 
 const LoginProviders = () => {
+  const router = useRouter();
   return (
     <div>
         <div className="flex items-center justify-center gap-6">
-            <ButtonIcon onClick={()=>signIn("google")} label="Google" icon={<FaGoogle/>} className="flex flex-row-reverse items-center gap-1 bg-slate-600 rounded-lg p-3 hover:bg-slate-700 duration-150"/>
-            <ButtonIcon onClick={()=>signIn("github")} label="Github" icon={<FaGithub/>} className="flex flex-row-reverse items-center gap-1 bg-slate-600 rounded-lg p-3 hover:bg-slate-700 duration-150"/>
+            <ButtonIcon onClick={()=>{
+              signIn("google")
+              router.push("/");
+            }} label="Google" icon={<FaGoogle/>} className="flex flex-row-reverse items-center gap-1 bg-slate-600 rounded-lg p-3 hover:bg-slate-700 duration-150"/>
+            <ButtonIcon onClick={()=>{
+              signIn("github")
+              router.push("/");
+            }} label="Github" icon={<FaGithub/>} className="flex flex-row-reverse items-center gap-1 bg-slate-600 rounded-lg p-3 hover:bg-slate-700 duration-150"/>
         </div>
     </div>
   )
